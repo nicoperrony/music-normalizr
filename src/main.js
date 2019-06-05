08bf31cfae2f4d3a5e175561e1adca16e2d5a274
nicoperrony <nicolas.perrony@gmail.com>
2019-06-05 22:11:35 +0200

    Refactoring local storage usage

--- src/main.js
+++ src/main.js
@@ -3,18 +3,13 @@ import "./plugins/vuetify";
 import App from "./App.vue";
 import { Router } from "./router";
 
-import { DISCOGS_KEY, DISCOGS_SECRET, FILE_NAME_PATTERN } from "./storage";
+import { Storage } from "./storage";
 
 Vue.config.productionTip = false;
+
+Storage.initialize();
 
 new Vue({
   router: Router,
   render: h => h(App)
 }).$mount("#app");
-
-localStorage.setItem(DISCOGS_KEY, "sdzUjQTGFfPrePmXrYcY");
-localStorage.setItem(DISCOGS_SECRET, "yUZTmWvDqWjozLldGXrpmBtzuYqLXcrh");
-localStorage.setItem(
-  FILE_NAME_PATTERN,
-  "[track-num]. [artist] feat [featuring] - [title]"
-);
--- src/storage/index.js
+++ src/storage/index.js
@@ -3,3 +3,17 @@ const DISCOGS_SECRET = "DISCOGS_SECRET";
 const FILE_NAME_PATTERN = "FILE_NAME_PATTERN";
 
 export { DISCOGS_KEY, DISCOGS_SECRET, FILE_NAME_PATTERN };
+
+export class Storage {
+  static initialize() {
+    localStorage.setItem(DISCOGS_KEY, "sdzUjQTGFfPrePmXrYcY");
+    localStorage.setItem(DISCOGS_SECRET, "yUZTmWvDqWjozLldGXrpmBtzuYqLXcrh");
+
+    if (!localStorage.getItem(FILE_NAME_PATTERN)) {
+      localStorage.setItem(
+        FILE_NAME_PATTERN,
+        "[track-num]. [artist] feat [featuring] - [title]"
+      );
+    }
+  }
+}
