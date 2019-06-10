3686fc9571c17b22030d3e20347c6fe669a25868
nicoperrony <nicolas.perrony@gmail.com>
2019-06-10 22:36:35 +0200

    Remove useless template tag

--- src/components/MusicAlbumSelector.vue
+++ src/components/MusicAlbumSelector.vue
@@ -10,17 +10,13 @@
       @input="selectAlbum"
     >
       <template v-slot:item="data">
-        <template>
-          <v-list-tile-avatar>
-            <img :src="data.item.cover" />
-          </v-list-tile-avatar>
-          <v-list-tile-content>
-            <v-list-tile-title>{{ data.item.title }}</v-list-tile-title>
-            <v-list-tile-sub-title>{{
-              data.item.artist
-            }}</v-list-tile-sub-title>
-          </v-list-tile-content>
-        </template>
+        <v-list-tile-avatar>
+          <img :src="data.item.cover" />
+        </v-list-tile-avatar>
+        <v-list-tile-content>
+          <v-list-tile-title>{{ data.item.title }}</v-list-tile-title>
+          <v-list-tile-sub-title>{{ data.item.artist }}</v-list-tile-sub-title>
+        </v-list-tile-content>
       </template>
     </v-autocomplete>
 
