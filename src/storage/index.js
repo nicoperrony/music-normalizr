beed0cedcc8db1da2985701b7dff8752df40f689
nicoperrony <nicolas.perrony@gmail.com>
2019-06-11 00:07:39 +0200

    Remove discogs keys (will be use in a service)

--- src/storage/index.js
+++ src/storage/index.js
@@ -1,14 +1,9 @@
-const DISCOGS_KEY = "DISCOGS_KEY";
-const DISCOGS_SECRET = "DISCOGS_SECRET";
 const FILE_NAME_PATTERN = "FILE_NAME_PATTERN";
 
-export { DISCOGS_KEY, DISCOGS_SECRET, FILE_NAME_PATTERN };
+export { FILE_NAME_PATTERN };
 
 export class Storage {
   static initialize() {
-    localStorage.setItem(DISCOGS_KEY, "sdzUjQTGFfPrePmXrYcY");
-    localStorage.setItem(DISCOGS_SECRET, "yUZTmWvDqWjozLldGXrpmBtzuYqLXcrh");
-
     if (!localStorage.getItem(FILE_NAME_PATTERN)) {
       localStorage.setItem(
         FILE_NAME_PATTERN,
