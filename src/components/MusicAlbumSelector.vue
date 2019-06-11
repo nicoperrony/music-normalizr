953636123ed36e7d7c6c8c7f2b2537a18f422439
nicoperrony <nicolas.perrony@gmail.com>
2019-06-12 00:04:14 +0200

    Get album from Discogs

--- src/components/MusicAlbumSelector.vue
+++ src/components/MusicAlbumSelector.vue
@@ -1,21 +1,32 @@
 <template>
   <v-layout column>
     <v-autocomplete
-      v-model="selectedAlbumId"
+      v-model="album"
       :items="albums"
+      :search-input.sync="search"
+      :loading="isLoading"
+      :return-object="true"
       color="primary lighten-2"
-      label="Music Album"
+      label="Album"
       item-text="title"
-      item-value="id"
+      prepend-icon="search"
       @input="selectAlbum"
     >
+      <template v-slot:no-data>
+        <v-list-tile>
+          <v-list-tile-title>
+            No album found
+          </v-list-tile-title>
+        </v-list-tile>
+      </template>
+
       <template v-slot:item="data">
         <v-list-tile-avatar tile>
           <img :src="data.item.cover" />
         </v-list-tile-avatar>
         <v-list-tile-content>
           <v-list-tile-title>{{ data.item.title }}</v-list-tile-title>
-          <v-list-tile-sub-title>{{ data.item.artist }}</v-list-tile-sub-title>
+          <v-list-tile-sub-title>{{ data.item.year }}</v-list-tile-sub-title>
         </v-list-tile-content>
       </template>
     </v-autocomplete>
@@ -29,7 +40,7 @@
           <v-list-tile-content>
             <v-list-tile-title>{{ selectedAlbum.title }}</v-list-tile-title>
             <v-list-tile-sub-title>{{
-              selectedAlbum.artist
+              selectedAlbum.year
             }}</v-list-tile-sub-title>
           </v-list-tile-content>
 
@@ -45,6 +56,9 @@
 </template>
 
 <script>
+import { Discogs } from "../services/discogs";
+import _ from "lodash";
+
 export default {
   name: "MusicAlbumSelector",
   props: {
@@ -52,36 +66,15 @@ export default {
   },
   data() {
     return {
-      selectedAlbumId: undefined,
-      albums: [
-        {
-          id: 1,
-          title: "Pili Pili sur un croissant au beurre",
-          artist: "Gaël Faye",
-          cover:
-            "https://www.laboutiqueafricavivre.com/881-thickbox/gael-faye-pili-pili-sur-un-croissant-au-beurre.jpg"
-        },
-        {
-          id: 3,
-          title: "Rythmes et botanique",
-          artist: "Gaël Faye",
-          cover:
-            "https://www.thebackpackerz.com/wp-content/uploads/2017/04/810ZWRuWy9L._SL1181_.jpg"
-        },
-        {
-          id: 2,
-          title: "Des fleurs",
-          artist: "Gaël Faye",
-          cover:
-            "https://images.genius.com/186ec1400a212566fb3406042525e5e4.399x399x1.jpg"
-        }
-      ]
+      search: "",
+      album: undefined,
+      isLoading: false,
+      albums: []
     };
   },
   methods: {
-    selectAlbum(id) {
-      const album = this.albums.find(album => album.id == id);
-      this.$emit("update:selectedAlbum", album);
+    selectAlbum() {
+      this.$emit("update:selectedAlbum", this.album);
     },
     unselectAlbum() {
       this.$emit("update:selectedAlbum", undefined);
@@ -90,9 +83,21 @@ export default {
   watch: {
     selectedAlbum(newVal) {
       if (newVal === undefined) {
-        this.selectedAlbumId = undefined;
+        this.album = undefined;
+        this.albums = [];
       }
-    }
+    },
+    search: _.debounce(function() {
+      if (this.search && this.selectedAlbum == undefined) {
+        this.isLoading = true;
+        Discogs.searchAlbum(this.search).then(
+          function(res) {
+            this.albums = res;
+            this.isLoading = false;
+          }.bind(this)
+        );
+      }
+    }, 1000)
   }
 };
 </script>
---
+++ src/services/discogs.js
@@ -0,0 +1,49 @@
+import axios from "axios";
+
+const DISCOGS_KEY = "sdzUjQTGFfPrePmXrYcY";
+const DISCOGS_SECRET = "yUZTmWvDqWjozLldGXrpmBtzuYqLXcrh";
+const DISCOGS_URL = "https://api.discogs.com";
+
+axios.interceptors.request.use(function(config) {
+  return {
+    ...config,
+    params: {
+      ...config.params,
+      key: DISCOGS_KEY,
+      secret: DISCOGS_SECRET,
+      page: 1,
+      per_page: 5
+    }
+  };
+});
+
+export class Discogs {
+  static async searchAlbum(query) {
+    try {
+      const response = await axios.get(DISCOGS_URL + "/database/search", {
+        params: {
+          q: query,
+          type: "master"
+        }
+      });
+      return Discogs._parseAlbum(response.data);
+    } catch (error) {
+      return [];
+    }
+  }
+
+  static _parseAlbum(data) {
+    return data.results.reduce((albums, row) => {
+      if (row.type == "master") {
+        albums.push({
+          id: row.id,
+          cover: row.cover_image,
+          title: row.title,
+          year: row.year
+        });
+      }
+
+      return albums;
+    }, []);
+  }
+}
